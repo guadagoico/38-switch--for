8b339091659841f4764e5c902b0f009fa94afb61
Guada <guadagoico@gmail.com>
2019-11-06 07:32:29 -0300

    (ejercicio 1 y 3)

--- if-else.js
+++ if-else.js
@@ -21,51 +21,76 @@
 
 //SWITCH
 
-let dia = prompt("Ingrese un dia")
+//let dia = prompt("Ingrese un dia")
 
-if (dia === "lunes"){
+//if (dia === "lunes"){
 
-}
-switch (dia) {
-  case "lunes":
-    alert("Su nombre proviene de la luna. En latín, se lo llamaba 'dies Lūnae'")
-    break;
-  case "martes":
-    alert("Su nombre proviene del planeta Marte. En latín, se lo llamaba 'dies Martis'")
-    break;
-  case "miercoles":
-    alert("Su nombre proviene del planeta Mercurio. En latín, se lo llamaba 'dies Mercuriī'")
-    break;
-  case "jueves":
-    alert("Su nombre proviene del planeta Mercurio. En latín, se lo llamaba 'dies Iovis'")
-    break;
-  case "Viernes":
-    alert("Su nombre proviene del planeta Venus. En latín, se lo llamaba 'dies Veneris'")
-    break;
-  case "Sabado":
-    alert("Su nombre proviene del Shabat hebreo, el día de descanso del pueblo judío")
-    break;
-  case "domingo":
-    alert("Su nombre es cristiano y proviene de la palabra latina 'Dominica', que significa 'señoril', por ser 'el día del Señor'")
-    break;
-  default:
-alert("Usted no ingreso un dia valido")
-    break;
-}
+//}
+//switch (dia) {
+//  case "lunes":
+//    alert("Su nombre proviene de la luna. En latín, se lo llamaba 'dies Lūnae'")
+//    break;
+//  case "martes":
+//    alert("Su nombre proviene del planeta Marte. En latín, se lo llamaba 'dies Martis'")
+//    break;
+//  case "miercoles":
+//    alert("Su nombre proviene del planeta Mercurio. En latín, se lo llamaba 'dies Mercuriī'")
+//    break;
+//  case "jueves":
+//    alert("Su nombre proviene del planeta Mercurio. En latín, se lo llamaba 'dies Iovis'")
+//    break;
+//  case "Viernes":
+//    alert("Su nombre proviene del planeta Venus. En latín, se lo llamaba 'dies Veneris'")
+//    break;
+//  case "Sabado":
+//    alert("Su nombre proviene del Shabat hebreo, el día de descanso del pueblo judío")
+//    break;
+//  case "domingo":
+//    alert("Su nombre es cristiano y proviene de la palabra latina 'Dominica', que significa 'señoril', por ser 'el día del Señor'")
+//    break;
+//  default:
+//alert("Usted no ingreso un dia valido")
+//    break;
+//}
 
 
-let animal = prompt("Ingrese un animal")
+//let animal = prompt("Ingrese un animal")
 
-switch (animal) {
-  case "jirafa":
-  case "leon":
-  case "orangutan":
-  alert("El animal vive en la sabana")
-  break;
-  case "gallina":
-  case "vaca":
-  case "Oveja":
-    alert("El animal vive en la granja")
-    break;
-}
+//switch (animal) {
+// case "jirafa":
+//  case "leon":
+//  case "orangutan":
+//  alert("El animal vive en la sabana")
+//  break;
+//  case "gallina":
+//  case "vaca":
+//  case "Oveja":
+//    alert("El animal vive en la granja")
+//    break;
+// }
+
+//1 Escribamos un programa que reciba
+// el numero de un mes via prompt
+// y le devuelva al usuario un alerta diciendole
+// cuantos dias tiene un mes
+//TIP mejorar declarando variables
+
+//FOR
+// for (let i= 0; i <= 10; i+=2)  // for (let i= 0; i <= 10; i++)
+// {
+//   console.logo(i)
+// }
+
+//Mostrar en consola los números del 0 al 10
+for (let i = 0; i <= 11; i++) {
+   console.log(i)
+ }
+
+//Mostrar en consola los números del 100 al 0
+//for (let i = 0; i >= 100; i++) {
+//  console.log(i)
+//}
+//Mostrar en consola los números pares del 0 al 1000
+for (let i = 0; i <= 1000; i+=2) {
+  console.log(i)
 }
